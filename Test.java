43e96d3f3daf2b92bc1b96f423c995a919fdf58a
Houba3 <161231255+Houba3@users.noreply.github.com>
2024-02-29 00:07:00 +0200

    ba2et ze2ab

--- Test.java
+++ Test.java
@@ -1,7 +1,7 @@
 public class Test {
     public static void main(String args[]) {
 
-        System.out.println("Hello ashez from Zeina");
+        System.out.println("Hello ashez from Ze2ab");
 
     }
 
